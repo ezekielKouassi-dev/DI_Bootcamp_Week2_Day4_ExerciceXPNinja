b8d3e93863cf8c256033e422de588abd53ab0783
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2023-01-01 23:42:00 +0000

    fix_exercice4

--- script.js
+++ script.js
@@ -132,3 +132,38 @@ function isPalindrome(character) {
 isPalindrome("madam");
 
 /* ******************* End Exercice 3 ********************** */
+
+/* ******************* Exercice 4 : Biggest Number ************** */
+
+// 1 - create function called biggestNumberInArray(arrayNumber) that takes an array as a parameter and returns the biggest number
+
+function biggestNumberInArray(arrayNumber) {
+
+    /**
+     * @description : cette fonction permet trouver la plus grande valeur dans un tableau
+     * et la retourne
+     * @param arrayNumber -> le tableau à trier
+     * @param biggestNumber -> 
+     */
+
+    if(arrayNumber.length > 0) {
+
+        let biggestNumber = 0;
+
+        for(let i of arrayNumber) {
+
+            if(i > biggestNumber) {
+
+                biggestNumber = i;
+
+            }
+        }
+        return biggestNumber;
+    }
+    return 0;
+
+}
+
+biggestNumberInArray(['a', 3, 4, 2]);
+
+/* ***************** End Exercice 4 ************** */
