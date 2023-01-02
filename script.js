b0b0850878a02692ba62a24080869cd6b9733dee
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2023-01-02 06:43:50 +0000

    fix exercice 5

--- script.js
+++ script.js
@@ -167,3 +167,22 @@ function biggestNumberInArray(arrayNumber) {
 biggestNumberInArray(['a', 3, 4, 2]);
 
 /* ***************** End Exercice 4 ************** */
+
+/* ***************** Exercice 5 ************** */
+
+// Write a JS function that takes an array and returns a new array with only unique elements
+
+function uniqueElements(array) {
+
+    /**
+     * @description cette fonction permet de supprimer les doublons dans une liste
+     */
+
+    let uniqueElementsList = array.filter((x, i) => array.indexOf(x) === i);
+    return uniqueElementsList;
+
+}
+
+uniqueElements([1,2,3,3,3,3,4,5]);
+
+/* *************** End Exercice 5 *************** */
